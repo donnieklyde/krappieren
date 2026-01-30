95527e6f8b7913d9d45e4b40013c657f6afad5f5
donnieklyde <donnieklyde@gmail.com>
2026-01-30 19:39:09 +0100

    Fix missing Link import

--- app/bosses/page.js
+++ app/bosses/page.js
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { usePosts } from "../context/PostsContext";
 import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";

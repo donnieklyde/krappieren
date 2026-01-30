b3567b23b0b38115ed704840404a0ac32135053c
donnieklyde <donnieklyde@gmail.com>
2026-01-30 22:35:08 +0100

    deploy step

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import styles from "./Navigation.module.css";
 import { usePathname, useRouter } from "next/navigation";
 
